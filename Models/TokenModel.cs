0efbf027458b3165d9b7d2aeb28fa1e406e514d6
Dmitry <dima946762@gmail.com>
2019-08-28 22:15:50 +0300

    Add token model

---
+++ Models/TokenModel.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using Twilio.Http;
+
+namespace ABM_CMS.Models
+{
+    public class TokenModel
+    {
+        [Key] public string Id { get; set; }
+
+        /// <summary>
+        /// The clientId where it comes from.
+        /// </summary>
+        [Required]
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Value of the token.
+        /// </summary>
+        [Required]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Get the token creation date.
+        /// </summary>
+        [Required]
+        public DataType CreatedDate { get; set; }
+
+        /// <summary>
+        /// The user it was issued to.
+        /// </summary>
+        [Required]
+        public string UserId { get; set; }
+        
+        [Required] public DataType LastModifiedDate { get; set; }
+        [Required] public DataType ExpiryTime { get; set; }
+    }
+}
