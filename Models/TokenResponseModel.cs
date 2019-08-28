9de9984cf45f3080fd79f52e96cc656a3c611c73
Dmitry <dima946762@gmail.com>
2019-08-28 22:30:52 +0300

    Add TokenResponseModel

---
+++ Models/TokenResponseModel.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ABM_CMS.Models
+{
+    public class TokenResponseModel
+    {
+        /// <summary>
+        /// JWT Token value.
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// Expiration time for the token.
+        /// </summary>
+        public DateTime Expiration { get; set; }
+        /// <summary>
+        /// RefreshToken value.
+        /// </summary>
+        public string RefreshToken { get; set; }
+        /// <summary>
+        /// User role.
+        /// </summary>
+        public string Roles { get; set; }
+        /// <summary>
+        /// User Name.
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}
